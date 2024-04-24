2d58d6eafd0dac6e5787f42ca8796d4c2df1abf3
Arsen Shkrumelyak <arsen@nerverift.com>
2024-04-24 18:59:35 +0100

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# zero portfolio
+
+i don’t like portfolios, but i made one anyway.
+
+it’s very simple—probably too simple.
+
+so, here’s the portfolio, enjoy.
---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "arsen shkrumelyak - i build things",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className + " " + "bg-zinc-50 dark:bg-zinc-950"}>
+        {children}
+      </body>
+    </html>
+  );
+}
