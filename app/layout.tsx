c9d0067c7b10682742ec49ef3ec8230d872b7d2b
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2024-08-26 12:54:03 +0100

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import { Inter } from "next/font/google";
 // Styles
 import "@/styles/globals.css";
 
+// Analytics
+import { Analytics } from "@vercel/analytics/react";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -21,6 +24,7 @@ export default async function RootLayout({
 		<html lang="en">
 			<body className={`${inter.className} bg-zinc-50 dark:bg-zinc-950`}>
 				{children}
+				<Analytics />
 			</body>
 		</html>
 	);
